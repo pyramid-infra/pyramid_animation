37e04795b4a9b8a1d2824d51495ee3e167770ef4
Fredrik Noren <fredrik.jw.noren@gmail.com>
2015-08-29 00:28:49 -0400

    Use ISystem name

--- src/lib.rs
+++ src/lib.rs
@@ -24,19 +24,14 @@ impl AnimationSubSystem {
     }
 }
 
-#[no_mangle]
-pub fn new() -> Box<SubSystem> {
-    Box::new(AnimationSubSystem::new())
-}
 
+impl ISubSystem for AnimationSubSystem {
 
-impl SubSystem for AnimationSubSystem {
-
-    fn on_entity_added(&mut self, system: &mut System, entity_id: &EntityId) {
+    fn on_entity_added(&mut self, system: &mut ISystem, entity_id: &EntityId) {
         let prop_refs: Vec<PropRef> = { system.get_properties(&entity_id).unwrap() };
         self.on_property_value_change(system, &prop_refs);
     }
-    fn on_property_value_change(&mut self, system: &mut System, prop_refs: &Vec<PropRef>) {
+    fn on_property_value_change(&mut self, system: &mut ISystem, prop_refs: &Vec<PropRef>) {
         for pr in prop_refs.iter().filter(|pr| pr.property_key == "animation") {
             let pn = system.get_property_value(&pr.entity_id, &pr.property_key.as_str()).unwrap();
             let anim = Animation::from_prop_node(&pn).unwrap();
@@ -44,7 +39,7 @@ impl SubSystem for AnimationSubSystem {
             self.animations.insert(pr.entity_id, (anim, target));
         }
     }
-    fn update(&mut self, system: &mut System, delta_time: time::Duration) {
+    fn update(&mut self, system: &mut ISystem, delta_time: time::Duration) {
         for (_, &mut (ref mut animation, ref mut target)) in self.animations.iter_mut() {
             let value = { animation.update(delta_time) };
             system.set_property(&target.entity_id.clone(), target.property_key.clone(), PropNode::Float(value));
